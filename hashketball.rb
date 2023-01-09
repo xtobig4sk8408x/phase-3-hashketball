4e2bb5a5ec6b932b91bfc0d31e12e3d59ef73af1
Theodore John Josef <john.josef@aol.com>
2023-01-09 15:53:15 -0800

    commit tree 76f9045db8f85dcf76b730824a57c01828c2a815

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -127,3 +128,51 @@ def game_hash
 end
 
 # Write code here
+
+def all_players 
+  game_hash[:home][:players].concat(game_hash[:away][:players])
+end
+
+def num_points_scored(name)
+  # player_found = nil
+  all_players.each do |player|
+    if player[:player_name] == name
+      return player[:points] 
+    end
+  end
+  return "Player not found"
+end
+
+def shoe_size(name)
+  player_found = all_players.find {|player| player[:player_name] == name}
+  player_found ? player_found[:shoe] : "who?"
+end
+
+def team_colors(team_name)
+  if game_hash[:home][:team_name] == team_name
+    game_hash[:home][:colors]
+  elsif game_hash[:away][:team_name] == team_name
+    game_hash[:away][:colors]
+  else
+    return "We don't have that team!"
+  end
+end
+
+def team_names
+  return [game_hash[:home][:team_name], game_hash[:away][:team_name]]
+end
+
+def player_numbers(team_name)
+  if game_hash[:home][:team_name] == team_name
+    return game_hash[:home][:players].map {|player| player[:number]}
+  elsif game_hash[:away][:team_name] == team_name
+    return game_hash[:away][:players].map {|player| player[:number]}
+  end
+  return "The team doesn't exist"
+end
+
+def winning_team
+  home_score = game_hash[:home][:players].sum {|player| player[:points]}
+  away_score = game_hash[:away][:players].sum {|player| player[:points]}
+  home_score = game_hash[:home][:players].sum {|player| player[:points]}
+end
